21e7373b0a8b998dbb7150d3f98ab9ff759fc9a8
Demonica22 <demonica2003@mail.ru>
2022-11-16 00:55:56 +0300

    -first version of replacement in mail text done

--- mails/urls.py
+++ mails/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'mails'
 urlpatterns = [
     path('inbox', views.InboxPage.as_view(), name='inbox'),
-    path('<int:id>', views.MailDetail.as_view(), name='detail'),
+    path('<int:id>', views.MailDetailPage.as_view(), name='detail'),
+    #path('compose',views.)
 ]
--- mails/views.py
+++ mails/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Mail
+import re
 
 
 class InboxPage(generic.ListView):
@@ -10,12 +11,14 @@ class InboxPage(generic.ListView):
     def get(self, request):
         try:
             mails = Mail.objects.all().filter(receiver_id=request.user.id)
+            mails = fullfill_mails(mails)
         except Mail.DoesNotExist:
             mails = []
+
         return render(request, self.template_name, {'mails': mails})
 
 
-class MailDetail(generic.DetailView):
+class MailDetailPage(generic.DetailView):
     template_name = 'mails/inbox.html'
     context_object_name = 'detail_page'
 
@@ -24,6 +27,18 @@ class MailDetail(generic.DetailView):
             mail = [Mail.objects.get(id=self.kwargs["id"])]
             mail[0].was_read = True
             mail[0].save()
+            mail = fullfill_mails(mail)
         except Mail.DoesNotExist:
             mail = None
         return render(request, self.template_name, {'mails': mail})
+
+
+def fullfill_mails(mails):
+    # TODO: change default values of replacement
+    regex = r"[%]\b.*\b[%]"
+    for mail in mails:
+        if re.search(regex, str(mail.text)):
+            mail.text = mail.text.replace("%first_name%", mail.receiver.first_name or "default")
+            mail.text = mail.text.replace("%last_name%", mail.receiver.last_name or "default")
+            mail.text = mail.text.replace("%birthday%", mail.receiver.birth_date or "default")
+    return mails
